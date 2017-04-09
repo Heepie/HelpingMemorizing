3fc249d4b3caf213b588b30d7c76a43de45fcb8e
heepie <gheepie@gmail.com>
2017-04-09 21:34:42 +0900

    April 09 commit. Issue: Alarm

--- app/src/main/java/kr/co/heepie/helpingmemorizingapp/alarm/NotificationService.java
+++ app/src/main/java/kr/co/heepie/helpingmemorizingapp/alarm/NotificationService.java
@@ -1,9 +1,16 @@
 package kr.co.heepie.helpingmemorizingapp.alarm;
 
+import android.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.util.Log;
+
+import kr.co.heepie.helpingmemorizingapp.form.DetialCardInfo;
 
 /**
  * Created by Hee_Ju.M on 2017-04-04.
@@ -11,9 +18,80 @@ import android.support.annotation.Nullable;
 
 public class NotificationService extends Service {
 
-    @Nullable
+    /**
+     * Class for clients to access
+     */
+    public class ServiceBinder extends Binder {
+        NotificationService getService() {
+            return NotificationService.this;
+        }
+    }
+
+    // Unique id to identify the notification.
+    private static final int NOTIFICATION = 123;
+    // Name of an intent extra we can use to identify if this service was started to create a notification
+    public static final String INTENT_NOTIFY = "Heepie";
+    // The system notification manager
+    private NotificationManager mNM;
+
+    @Override
+    public void onCreate() {
+        Log.i("NotifyService", "onCreate()");
+        mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+
+        // If this service was started by out AlarmTask intent then we want to show our notification
+        if(intent.getBooleanExtra(INTENT_NOTIFY, false))
+            showNotification();
+
+        // We don't care if this service is stopped as we have already delivered our notification
+        return START_NOT_STICKY;
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    // This is the object that receives interactions from clients
+    private final IBinder mBinder = new ServiceBinder();
+
+    /**
+     * Creates a notification and shows it in the OS drag-down status bar
+     */
+    private void showNotification() {
+        // This is the 'title' of the notification
+        CharSequence title = "Alarm!!";
+        // This is the icon to use on the notification
+        int icon = R.drawable.ic_dialog_alert;
+        // This is the scrolling text of the notification
+        CharSequence text = "Your notification time is upon us.";
+        // What time to show on the notification
+        long time = System.currentTimeMillis();
+
+//        Notification notification = new Notification(icon, text, time);
+
+
+
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, DetialCardInfo.class), 0);
+
+        Notification notification = new Notification.Builder(this)
+                                                    .setContentTitle(title)
+                                                    .setContentText(text)
+                                                    .setSmallIcon(icon)
+                                                    .setContentIntent(contentIntent)
+                                                    .build();
+
+        // Send the notification to the system.
+        mNM.notify(NOTIFICATION, notification);
+
+        // Stop the service when we are finished
+        stopSelf();
     }
 }
+
